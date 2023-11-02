183c5f41fc1b6654b59e496b6168726953bc9600
Nana Janashia <nana.janashia@nnsoftware.at>
2023-11-02 16:15:36 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,6 @@
 terraform {
   required_version = ">= 0.12"
-  backend "s3" {
-    bucket = "myapp-tf-s3-bucket"
-    key = "myapp/state.tfstate"
-    region = "eu-central-1"
-  }
+  backend "local" {}
 }
 
 provider "aws" {
